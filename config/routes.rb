0713ea800f758437a14611116cec6b8b07aa007f
Faisal Hassan <faisal.hassanx@gmail.com>
2016-12-11 01:37:36 +0000

    Generate friendships model

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'friends', to: "users#friends" 
   get 'search', to: 'stocks#search'
   get 'portfolio', to: 'users#portfolio'
   root 'pages#index'
